4c7a7e0a48387fa349d8326788c4764abd3fad20
Mingjie Jiang <13333578+itsmingjie@users.noreply.github.com>
2022-11-30 01:32:55 -0600

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,30 @@ const Page = (): JSX.Element => {
       <div className="flex flex-col gap-2">
         <h1 className="text-4xl font-medium">Heyo!</h1>
         <p className="max-w-md text-xs font-light leading-5 tracking-wide text-light/80">
+          I'm Mingjie, a web & product engineer at{" "}
+          <Link
+            href="https://www.crunchbase.com/organization/ender-3c26"
+            external
+            className="hover:underline text-light"
+          >
+            Ender
+          </Link>{" "}
+          <span className="text-hint">(we might be hiring!)</span>. I love
+          spending most of my day fiddling with UI in Figma and building ✨
+          shiny ✨ animations.
+        </p>
+        <p className="max-w-md text-xs font-light leading-5 tracking-wide text-light/80">
+          I do charity work through{" "}
+          <Link
+            href="https://executebig.org"
+            external
+            className="hover:underline text-light"
+          >
+            Execute Big
+          </Link>
+          .
+        </p>
+        <p className="max-w-md text-xs font-light leading-5 tracking-wide text-light/80">
           I'm currently rebuilding my website, so things might look empty or
           broken around here. If you're interested in the progress, you can find
           the source code on{" "}
